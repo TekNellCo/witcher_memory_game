eee6120f42961053284e72b764284a6f45dbc9d1
Nelson <129366078+TekNellCo@users.noreply.github.com>
2024-12-04 15:49:29 -0500

    add indivitual card flip

--- src/gameBoard.jsx
+++ src/gameBoard.jsx
@@ -4,47 +4,37 @@ import cardCreatorShuffle from './cards';
 
 function GameBoard() {
   const [shuffledCards, setShuffledCards] = useState([]); ///stored shuffled cards
+  const [isHidden, setIsHidden] = useState([]); ///tracks if cards are hidden or not
 
   /////pulls shuffled cards from cards.jsx
   useEffect(() => {
     const pulledCards = cardCreatorShuffle();
     setShuffledCards(pulledCards);
+    setIsHidden(new Array(pulledCards.length).fill(true));
     console.log('shuffle cards pulled', pulledCards);
   }, []);
 
   const cardBackground = '/src/assets/card_background.webp'; ///default card background
-  const [isHidden, setIsHidden] = useState(true); ///is card facedown or up
 
-  function handleCardClick() {
-    ////switches from facedown to up
-    setIsHidden((previous) => !previous);
+  function handleCardClick(index) {
+    ////switches from facedown to up and vice-verse
+    setIsHidden((previous) => previous.map((hidden, i) => (i === index ? !hidden : hidden)));
   }
   return (
     <>
       <div className="gameContainer">
-        {/* ////////////////// */}
-        <div className="cardContainer" onClick={handleCardClick}>
-          <div className={`card ${isHidden ? 'flipped' : ''}`}>
-            <div className="front">
-              <img src={cardBackground} alt="" />
-            </div>
-            <div className="back">
-              <img src={testCard} alt="" />
-            </div>
-          </div>
-        </div>
-        {/* ////////////////// */}
-        <div className="cardContainer" onClick={handleCardClick}>
-          <div className={`card ${isHidden ? 'flipped' : ''}`}>
-            <div className="front">
-              <img src={cardBackground} alt="" />
-            </div>
-            <div className="back">
-              <img src={testCard} alt="" />
+        {shuffledCards.map((cards, index) => (
+          <div key={index} className="cardContainer" onClick={() => handleCardClick(index)}>
+            <div className={`card ${isHidden[index] ? '' : 'flipped'}`}>
+              <div className="front">
+                <img src={cardBackground} alt="" />
+              </div>
+              <div className="back">
+                <img src={cards.src} alt="" />
+              </div>
             </div>
           </div>
-        </div>
-        {/* ////////////////// */}
+        ))}
       </div>
     </>
   );
