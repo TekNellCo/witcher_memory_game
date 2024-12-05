bb51c69fc8f1daf1592213e442a556cc65dd35f1
Nelson <129366078+TekNellCo@users.noreply.github.com>
2024-12-04 21:48:22 -0500

    checks if flipped card matches,if not flips back

--- src/gameBoard.jsx
+++ src/gameBoard.jsx
@@ -3,34 +3,59 @@ import { useState, useEffect } from 'react';
 import cardCreatorShuffle from './cards';
 
 function GameBoard() {
+  const cardBackground = '/src/assets/card_background.webp'; ///default card background
   const [shuffledCards, setShuffledCards] = useState([]); ///stored shuffled cards
   const [isHidden, setIsHidden] = useState([]); ///tracks if cards are hidden or not
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [isFlipped, setFlippedCards] = useState([]);
 
   /////pulls shuffled cards from cards.jsx
   useEffect(() => {
     const pulledCards = cardCreatorShuffle();
     setShuffledCards(pulledCards);
-    setIsHidden(new Array(pulledCards.length).fill(true));
+    setIsHidden(new Array(pulledCards.length).fill(true)); /////puts all the cards in a hidden array
     console.log('shuffle cards pulled', pulledCards);
   }, []);
 
-  const cardBackground = '/src/assets/card_background.webp'; ///default card background
+  function handleCardClick(card, index) {
+    if (matchedCards.includes(card)) return;
 
-  function handleCardClick(index) {
-    ////switches from facedown to up and vice-verse
-    setIsHidden((previous) => previous.map((hidden, i) => (i === index ? !hidden : hidden)));
+    setFlippedCards((currentFlipped) => {
+      const updateFlipped = [...currentFlipped, card];
+
+      if (updateFlipped.length === 2) {
+        const [firstCard, secondCard] = updateFlipped;
+        if (firstCard.name === secondCard.name) {
+          setMatchedCards((currenMatch) => [...currenMatch, firstCard, secondCard]);
+        } else {
+          setTimeout(() => {
+            setIsHidden((hiddenValue) =>
+              hiddenValue.map((hidden, i) => (shuffledCards[i] === firstCard || shuffledCards[i] === secondCard ? true : hidden))
+            );
+          }, 800);
+        }
+        return [];
+      }
+      return updateFlipped;
+    });
+
+    // console.log('1st card', firstCard, '2nd card', secondCard);
+
+    // console.log('index log', index, 'card log', card.name);
+    setIsHidden((hiddenValue) => hiddenValue.map((hidden, i) => (i === index ? !hidden : hidden)));
   }
+
   return (
     <>
       <div className="gameContainer">
-        {shuffledCards.map((cards, index) => (
-          <div key={index} className="cardContainer" onClick={() => handleCardClick(index)}>
+        {shuffledCards.map((card, index) => (
+          <div key={index} className="cardContainer" onClick={() => handleCardClick(card, index)}>
             <div className={`card ${isHidden[index] ? '' : 'flipped'}`}>
               <div className="front">
                 <img src={cardBackground} alt="" />
               </div>
               <div className="back">
-                <img src={cards.src} alt="" />
+                <img src={card.src} alt="" />
               </div>
             </div>
           </div>
